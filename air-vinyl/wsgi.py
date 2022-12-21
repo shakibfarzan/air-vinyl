2321f79f090afb298b3d6173b183f9038f7bb1ee
shakibfarzan <shakib.farzan@protonmail.com>
2022-12-22 00:46:40 +0330

    change project name

--- air-vinyl/wsgi.py
+++ air-vinyl/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for spotifyapp project.
+WSGI config for air-vinyl project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spotifyapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'air-vinyl.settings')
 
 application = get_wsgi_application()
